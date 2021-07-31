b64640922d69dbda47ffbb24ef8fb3836d1db996
tanphat52 <81603126+tanphat52@users.noreply.github.com>
2021-07-31 13:09:58 +0700

    funtion findRange $..$

--- phat.cpp
+++ phat.cpp
@@ -1,6 +1,6 @@
 #include "function.h"
 
-bool isSubstring(string s1, string s2)
+bool searchEngine::isSubstring(string s1, string s2)
 {
     int M = s1.length();
     int N = s2.length();
@@ -18,7 +18,7 @@ bool isSubstring(string s1, string s2)
     return false;
 }
 
-bool checkOperator(string sen) {
+bool searchEngine::checkOperator(string sen) {
 	if (isSubstring("AND", sen) || isSubstring("OR", sen) || isSubstring("-", sen) || isSubstring("+", sen) )
 		return true;
 	if (isSubstring("filetype", sen) || isSubstring("intitle", sen) || isSubstring("~", sen))
@@ -27,4 +27,23 @@ bool checkOperator(string sen) {
 		return true;
 	return false;
 }
+
+void searchEngine::findRange(string query, double& low, double& high) {
+	size_t pos1 = query.find('$') + 1;
+	size_t pos2 = query.find("..$");
+	string num1 = query.substr(pos1, pos2 - pos1);
+	pos2 += 3;
+	stringstream ss1, ss2;
+	ss1 << num1;
+	ss1 >> low;
+	size_t pos3 = pos2 + 1;
+	while (query[pos3] >= 48 && query[pos3] <= 57) 
+		pos3++;
+	string num2 = query.substr(pos2, pos3 - pos2);
+	ss2 << num2;
+	ss2 >> high;
+}
+
+
+
 
