11a62a4d1175c627551d2a13d7152169a55a3eaa
Daike <81571936+OrijinaruDaike@users.noreply.github.com>
2021-08-01 21:18:38 +0700

    insert
    inserrt

--- Quan.cpp
+++ Quan.cpp
@@ -25,3 +25,23 @@ string searchEngine::floatToString(float num)
 	string out_string = ss.str();
 	return out_string;
 }
+
+void searchEngine::insertStopWord(TrieNode*& stopword, string word)
+{
+	int len = word.length();
+	int index;
+
+	TrieNode* cur = stopword;
+
+	for (int i = 0; i < len; ++i)
+	{
+		index = convert(word[i]);
+		if (index == -1)
+			continue;
+		if (cur->children[index] == nullptr)
+			cur->children[index] = createNode();
+		cur = cur->children[index];
+	}
+	cur->isEndOfWord = true;
+}
+
