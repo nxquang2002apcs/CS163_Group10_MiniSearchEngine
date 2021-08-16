5870db565f531fafb7a43f0a33cf80353a7f66ad
nxquang2002apcs <nxquang20@apcs.com>
2021-08-16 21:36:00 +0700

    Update main function

--- Main.cpp
+++ Main.cpp
@@ -1,31 +1,133 @@
 #include "Function.h"
+#include "ConsoleManagement.h"
 
-int main() {
+int main() {	
 	searchEngine engine;
+	
 	File file;
 	ifstream in;
-	TrieNode* root;
-	string path = "../Data/001.txt";
-	engine.inputFile(root, in, path);
-	file.name = "../Data/001.txt";
-	file.score = 0;
-
+	//TrieNode* root[100];
+	vector<TrieNode*> root;
+	vector<string> listName;
+	TrieNode* stopWord = engine.createNode();
+	string path = "../Search Engine-Data/";
+	clock_t start, stop;
+	start = clock();
+	engine.Initialize(root,  listName, stopWord, path);
+	stop = clock();
+	cout << "Time for indexing 100 files: " << stop - start << endl;;
+	
+	
+	int choice = 1;
 	string query;
-	cout << "QUERY: ";
-	getline(cin, query);
-	cout << "Result: ";
-	if (engine.searchTrie(root, nullptr, file.pos, file.score, query))
-		cout << "True" << endl;
-	else cout << "False" << endl;
-	cout << "Score: " << file.score << endl;
-	cout << "Position: ";
-	for (int i = 0; i < file.pos.size(); ++i)
-		cout << file.pos[i] << " ";
-	cout << endl;
-	cout << "===============================================================================" << endl;
-	cout << "001.txt" << endl;
-	engine.display(file);
+	int choice2;
+	while (choice != 3) {
+		engine.titleInterface(choice);
+		system("cls");
+		if (choice == 1)
+		{
+			engine.readInQuery(query);
+			engine.searchInterface(choice2);
 
-	engine.releaseTrie(root);
+			while (choice2 != 1 && choice2 != 2 && choice2 != 3)
+			{
+				system("cls");
+				gotoXY(15, 2);
+				cout << char(218);
+				for (int i = 0; i < 86; ++i)
+					cout << char(196);
+				cout << char(191);
+
+				gotoXY(15, 3); cout << char(179);
+				gotoXY(102, 3); cout << char(179);
+
+				gotoXY(15, 4);
+				cout << char(192);
+				for (int i = 0; i < 86; ++i)
+					cout << char(196);
+				cout << char(217);
+
+				gotoXY(17, 3);
+				cout << "QUERY: " << query;
+
+				engine.searchInterface(choice2);
+			}
+			if (choice2 == 1) {
+				system("cls");
+				gotoXY(15, 2);
+				cout << char(218);
+				for (int i = 0; i < 86; ++i)
+					cout << char(196);
+				cout << char(191);
+
+				gotoXY(15, 3); cout << char(179);
+				gotoXY(102, 3); cout << char(179);
+
+				gotoXY(15, 4);
+				cout << char(192);
+				for (int i = 0; i < 86; ++i)
+					cout << char(196);
+				cout << char(217);
+
+				gotoXY(17, 3);
+				cout << "QUERY: " << query;
+				cout << endl << endl;
+				engine.searchProcess(query, root, listName, stopWord);
+				system("pause");
+			} 
+			else if (choice2 == 2) {
+				system("cls");
+				gotoXY(15, 2);
+				cout << char(218);
+				for (int i = 0; i < 86; ++i)
+					cout << char(196);
+				cout << char(191);
+
+				gotoXY(15, 3); cout << char(179);
+				gotoXY(102, 3); cout << char(179);
+
+				gotoXY(15, 4);
+				cout << char(192);
+				for (int i = 0; i < 86; ++i)
+					cout << char(196);
+				cout << char(217);
+
+				gotoXY(17, 3);
+				cout << "QUERY: " << query;
+
+				engine.historySuggestionInterface(query);
+				system("cls");
+				gotoXY(15, 2);
+				cout << char(218);
+				for (int i = 0; i < 86; ++i)
+					cout << char(196);
+				cout << char(191);
+
+				gotoXY(15, 3); cout << char(179);
+				gotoXY(102, 3); cout << char(179);
+
+				gotoXY(15, 4);
+				cout << char(192);
+				for (int i = 0; i < 86; ++i)
+					cout << char(196);
+				cout << char(217);
+
+				gotoXY(17, 3);
+				cout << "QUERY: " << query << endl << endl;
+
+				engine.searchProcess(query, root, listName, stopWord);
+				system("pause");
+			}
+			if (choice2 == 3) continue;
+		}
+		if (choice == 2) {
+			string path = "history.txt";
+			engine.viewHistory(path);
+		}
+	}
+
+	for (int i = 0; i < root.size(); ++i)
+		engine.releaseTrie(root[i]);
+
 	return 0;
 }
